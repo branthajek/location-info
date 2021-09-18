fd04e72e844323b60bacf7f95e50bceacdd1d83c
Brant Hajek <branthajek@gmail.com>
2021-09-18 16:59:58 -0700

    fix firebase hosting

--- src/search-location.ts
+++ src/search-location.ts
@@ -15,14 +15,14 @@ export function searchLocationHandler(event: Event) {
         if (response.data.status != 'OK') {
             throw new Error('Could not fetch location');
         }
-        console.log(response)
+        // console.log(response)
         coordinates = response.data.results[0].geometry.location;
         const map = new google.maps.Map(Constants.mapElement, {
             center: coordinates,
             zoom: 8
         });
         new google.maps.Marker({position: coordinates, map: map});
-        console.log(coordinates);
+        // console.log(coordinates);
 
         const elevator = new google.maps.ElevationService;
         displayLocationElevation(coordinates, elevator);
@@ -32,26 +32,26 @@ export function searchLocationHandler(event: Event) {
             elevator.getElevationForLocations({
             'locations': [location]
             }, function(results: {elevation: number}[], _: any) {
-                console.log(results);
+                // console.log(results);
                 let elevationMeters = results[0].elevation;
                 let elevationFeet = (elevationMeters * 3.281).toFixed(2);
                 Constants.elevation.textContent = `Elevation at this point is ${elevationFeet} feet.`;
             });
         }
         axios.get<OpenWeatherResponse>(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lng}&units=imperial&appid=${WEATHER_API_KEY}`).then(response => {
-        console.log(response)
+        // console.log(response)
             Constants.weatherDescription.textContent = `Current weather condition: ${response.data.weather[0].description}.`;
             Constants.temperature.textContent = `Current temperature: ${response.data.main.temp.toString()} F.`;
         }).catch(error => {
             alert(error.message);
-            console.log(error);
+            // console.log(error);
         });
 
         Constants.locationInformation.classList.remove("d-none");
 
     }).catch(error => {
         alert(error.message);
-        console.log(error);
+        // console.log(error);
     });
 };
 
